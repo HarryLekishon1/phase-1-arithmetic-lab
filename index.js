382187bb512826886297d915888bfd6fdf5a09b0
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-07 11:14:44 +0300

    commit tree 55c91f9cf5a1fceb4d8e56749370dc84b01d3871

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+var num1 = 31;
+var num2 =2;
+const multiply = num1 * num2;
+
+
+
+var num3 = 24;
+var num4 = 5
+const mod = num3%num4;
+
+const max = Math.max(20,5,9);
+
+var random  = Math.floor(Math.random() * 10) + 1;
